60639427ee2bdf8c3c8fe41a6e21345d24a4b825
xseignard <xavier.seignard@gmail.com>
2019-01-11 19:31:57 +0100

    :beer: Toastr
    See #2

--- functions.php
+++ functions.php
@@ -162,6 +162,10 @@ function map_scripts()
 
     wp_enqueue_script('map-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true);
 
+    wp_enqueue_script('toastr', 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js');
+    wp_enqueue_style('toastr', 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css');
+
+
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
